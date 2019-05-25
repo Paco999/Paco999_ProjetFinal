7875e9173b3deed5c15adc5e8d68ee9c298dd20e
Paco999 <letourneaumaxime@carrefour.cegepvicto.ca>
2019-05-25 13:26:42 -0400

    2019-05-25

--- bootstrap/js/script.js
+++ bootstrap/js/script.js
@@ -1,18 +1,17 @@
 $("#boutonBellatorUFC").click(function() {
-    /*
+
     var texte = $('#boutonBellatorUFC').text();
     if (texte === "Bellator")
     {
         $("button").text("UFC");
-        alert("Changement texte")
+        $('h2').text("Champions de Bellator")
     }
     if (texte === "UFC")
     {
         $("button").text("Bellator");
-        alert("Changement texte")
-
+        $("h2").text("Champions de l'UFC");
     }
-    */
+
     var Heavy = $('#DanielCormier').attr("src");
     if (Heavy=== "img/UFC/DanielCormier.png")
     {
@@ -73,14 +72,14 @@ $("#boutonBellatorUFC").click(function() {
         $('#MaxHolloway').attr("src", "img/UFC/MaxHolloway.png");
     }
 
-    var Bantam = $('#MarlonMoraes').attr("src");
-    if (Bantam === "img/UFC/MarlonMoraes.png")
+    var Bantam = $('#Aucun').attr("src");
+    if (Bantam === "img/UFC/Aucun.png")
     {
-        $('#MarlonMoraes').attr("src", "img/Bellator/DarrionCaldwell.png");
+        $('#Aucun').attr("src", "img/Bellator/DarrionCaldwell.png");
     }
     if (Bantam === "img/Bellator/DarrionCaldwell.png")
     {
-        $('#MarlonMoraes').attr("src", "img/UFC/MarlonMoraes.png");
+        $('#Aucum').attr("src", "img/UFC/Aucun.png");
     }
 
     var Fly = $('#HenryCejudo').attr("src");
@@ -133,4 +132,33 @@ $("#boutonBellatorUFC").click(function() {
     {
         $('#JessicaAndrade').attr("src", "img/UFC/JessicaAndrade.png");
     }
+});
+
+AOS.init();
+// below listed default settings
+AOS.init({
+    // Global settings:
+    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: 'aos-init', // class applied after initialization
+    animatedClassName: 'aos-animate', // class applied on animation
+    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 120, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 400, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
+
+$("#boutonDark").click(function(){
+    $("p").css("background-color", "blue");
 });
